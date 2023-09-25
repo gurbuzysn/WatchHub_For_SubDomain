d0ca3617a6b9801521ba51824dd9d68608d7d7a6
yasinn gürbüz <yasingurbuz1@hotmail.com>
2023-09-25 23:24:15 +0300

    Add project files.

---
+++ src/Web/Interfaces/IBasketViewModelService.cs
@@ -0,0 +1,19 @@
+﻿namespace Web.Interfaces
+{
+    public interface IBasketViewModelService
+    {
+        Task<BasketViewModel> GetBasketViewModelAsync();
+
+        Task<BasketViewModel> AddItemToBasketAsync(int productId, int quantity);
+
+        Task EmptyBasketAsync();
+
+        Task RemoveItemAsync(int productId);
+
+        Task<BasketViewModel> UpdateQuantities(Dictionary<int, int> quantities);
+
+        Task TransferBasketAsync();
+
+        Task CheckoutAsync(string street, string city, string? state, string country, string zipCode);
+    }
+}
---
+++ src/Web/ViewComponents/NavbarBasketViewComponent.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Web.ViewComponents
+{
+    public class NavbarBasketViewComponent : ViewComponent
+    {
+        private readonly IBasketViewModelService _basketViewModelService;
+
+        public NavbarBasketViewComponent(IBasketViewModelService basketViewModelService)
+        {
+            _basketViewModelService = basketViewModelService;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var basket = await _basketViewModelService.GetBasketViewModelAsync();
+            return View(basket);
+        }
+    }
+}
